c93c3a7afcf3a9447b63d73d9e6efbc7c48a8946
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-11-10 18:55:22 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/layout/Navbar";
 
-function App() {
-  return <div>test</div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
 }
 
 export default App;
